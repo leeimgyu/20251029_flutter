026fcb2598191ee913ef264c5a5b619d120460de
leeimgyu <dldlarb12317@gmail.com>
2025-11-05 15:32:48 +0900

    20251105

--- dart_basic/lib/04_sync/01_future.dart
+++ dart_basic/lib/04_sync/01_future.dart
@@ -1,5 +1,51 @@
-// Future : **"지금 당장은 알 수 없지만, 미래의 어느 시점에 결과가 나올 것"**을 나타내는 객체
-// 시간이 오래 걸리는 작업(예: 인터넷에서 데이터 가져오기, 파일 읽기)을 처리할 때 사용
-void main() {
+void main() async {
+  Future<String> name;
+  Future<int> number;
+  Future<bool> isOpened;
+  Future<void> doNothing;
+  // addNumbers(1, 1);
 
+  // addNumbers2(1, 1);
+  // addNumbers2(2, 2);
+
+  // await addNumbers3(1, 1);
+  // await addNumbers3(2, 2);
+
+  final result1 = await addNumbers4(1, 1);
+  print('Result1: $result1');
+  final result2 = await addNumbers4(2, 2);
+  print('Result2: $result2');
+}
+
+void addNumbers(int num1, int num2) {
+  print('$num1 + $num2 계산 시작');
+  Future.delayed(Duration(seconds: 3), () {
+    print('$num1 + $num2 = ${num1 + num2}');
+  });
+  print('$num1 + $num2 계산 끝');
+}
+
+void addNumbers2(int num1, int num2) async {
+  print('$num1 + $num2 계산 시작');
+  await Future.delayed(Duration(seconds: 3), () {
+    print('$num1 + $num2 = ${num1 + num2}');
+  });
+  print('$num1 + $num2 계산 끝');
+}
+
+Future<void> addNumbers3(int num1, int num2) async {
+  print('$num1 + $num2 계산 시작');
+  await Future.delayed(Duration(seconds: 3), () {
+    print('$num1 + $num2 = ${num1 + num2}');
+  });
+  print('$num1 + $num2 계산 끝');
+}
+
+Future<int> addNumbers4(int num1, int num2) async {
+  print('$num1 + $num2 계산 시작');
+  await Future.delayed(Duration(seconds: 3), () {
+    print('$num1 + $num2 = ${num1 + num2}');
+  });
+  print('$num1 + $num2 계산 끝');
+  return num1 + num2;
 }
